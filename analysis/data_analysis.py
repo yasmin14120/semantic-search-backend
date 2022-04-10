dd3f08727bced953a53775897f0def7fb43bc18d
Yasmin <apuhan@droxit.de>
2022-04-10 13:02:56 +0200

    Add results

--- analysis/data_analysis.py
+++ analysis/data_analysis.py
@@ -1,4 +1,6 @@
 # Skript to analyse used dataset
+# Idea was to analyse the unique words count, the len of inverted index
+
 import json
 
 import requests
@@ -14,26 +16,20 @@ def count_unique_words(batch_size, index, fields):
     url = "http://localhost:9200/" + index + "/_pit?keep_alive=60m"
     pit_id = requests.post(url=url).json().get("id")
     hits = int(requests.get("http://localhost:9200/{}/_count".format(index)).json().get("count"))
-    from_, size_ = 10000, batch_size
-    old_avg_counter = 0
-    iteration_counter = 0
-    while hits > from_:
-        words, avg_word_count = search_after_query(from_, size_, fields, pit_id)
+    search_after = None
+    while hits > 0:
+        words, search_after, size_ = search_after_query(fields, pit_id, search_after)
         words = list(words)
-        old_avg_counter += avg_word_count
-        iteration_counter += 1
-        from_ += size_
         words = [words[i] for i in range(len(words)) if not check_for_doc(words[i], word_index)]
 
         bulk = prepare_bulk_index(words, word_index=word_index)
-        r = requests.post("http://localhost:9200/" + word_index + "/_doc/_bulk",
+        r = requests.post("http://localhost:9200/" + word_index + "/_bulk?refresh=wait_on",
                           headers={"content-type": "application/json"},
                           data=bulk)
-
-        print("DOCUMENTS SEEN: ", from_)
-        print("SUM ONLY AVG WORD COUNTER: ", old_avg_counter)
-        print("AVG: ", old_avg_counter/iteration_counter)
-        print("Iteration Counter")
+        print(r.text)
+        print(hits)
+        print(search_after)
+        hits -= size_
 
 
 def check_for_doc(word, index):
@@ -51,11 +47,10 @@ def check_for_doc(word, index):
     return False
 
 
-def search_after_query(from_, size_, fields, pit_id):
+def search_after_query(fields, pit_id, search_after=None):
     query = {
         "_source": fields,
-        "from": from_,
-        "size": size_,
+        "size": 10000,
         "query": {
             "match_all": {}
         },
@@ -67,12 +62,15 @@ def search_after_query(from_, size_, fields, pit_id):
             {"_id": {"order": "asc"}}
         ]
     }
+    if search_after is not None:
+        query["search_after"] = search_after
+
     url = "http://localhost:9200/_search"
     r = es_request("GET", url, data=query)
     res = r.json().get("hits", {}).get("hits", [])
     if len(res) == 0:
         print("ERROR: ", r.text)
-        return res, 0
+        return res, 0, 0
     strings = []
     for item in res:
         for field in fields:
@@ -88,7 +86,7 @@ def search_after_query(from_, size_, fields, pit_id):
             except Exception:
                 strings.append("")
 
-    return filter_words(strings)
+    return filter_words(strings), res[-1]["sort"], len(res)
 
 
 def filter_words(strings):
@@ -110,7 +108,8 @@ def filter_words(strings):
                 words.add(token.lemma_)
         counter_list.append(temp)
 
-    return words, sum(counter_list)/len(counter_list)
+    # Return counter_list to calculate the avg word per doc value
+    return words
 
 
 def prepare_bulk_index(word_list, word_index):
